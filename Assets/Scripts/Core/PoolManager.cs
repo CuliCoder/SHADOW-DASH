f7d898d9c648527d8aaa0f0fef7f76c745903e3f
trungnc113 <nguyencongtrung13@gmail.com>
2026-04-08 18:27:24 +0700

    updating pooling object

---
+++ Assets/Scripts/Core/PoolManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PoolManager : MonoBehaviour
+{
+    public static PoolManager Instance { get; private set; }
+    private Dictionary<string, Queue<GameObject>> poolDictionary = new Dictionary<string, Queue<GameObject>>();
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void prewarmPool(string key, GameObject prefab, int poolSize)
+    {
+        if (!poolDictionary.ContainsKey(key))
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i <= poolSize; i++)
+            {
+                GameObject gameObject = Instantiate(prefab);
+                gameObject.SetActive(false);
+                objectPool.Enqueue(gameObject);
+            }
+            poolDictionary.Add(key, objectPool);
+        }
+    }
+    public GameObject SpawnPool(string key, Vector2 pos, Quaternion rot)
+    {
+        if (!poolDictionary.ContainsKey(key))
+        {
+            Debug.LogWarning($"not found {key} in pool dictionary");
+            return null;
+        }
+        Queue<GameObject> pools = poolDictionary[key];
+        if (pools.Count == 0)
+        {
+            Debug.LogWarning($"{key} sold out");
+            return null;
+        }
+        GameObject objtoSpawn = pools.Dequeue();
+        objtoSpawn.transform.SetPositionAndRotation(pos, rot);
+        objtoSpawn.SetActive(true);
+        return objtoSpawn;
+    }
+    public void Despawn(string key, GameObject gameObject)
+    {
+        if (!poolDictionary.ContainsKey(key))
+        {
+            Debug.LogWarning($"not found {key} in pool dictionary");
+            Destroy(gameObject);
+            return;
+        }
+        gameObject.transform.SetPositionAndRotation(Vector3.zero,Quaternion.identity);
+        gameObject.SetActive(false);
+        poolDictionary[key].Enqueue(gameObject);
+    }
+}
---
+++ Assets/Scripts/Data/ObstacleBaseSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ObstacleBase", menuName = "ObstacleBase")]
+public class ObstacleBaseSO: ScriptableObject
+{
+    public string key;
+    public GameObject prefab;
+}
